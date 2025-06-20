0689c6ac54bb45d103c13022bec05c66035bec71
muhaimin365 <ab.muhaimin95@gmail.com>
2025-06-19 23:18:22 -0700

    first commit

--- Test.py
+++ Test.py
@@ -1,2 +1,23 @@
-print ("Muhaimin")
-print ("Hi")
+try:
+    UserInput=input("Enter value: ")
+    numerator = float(UserInput)
+    devidedValue = input("Enter Divided value: ")
+    denometor = float (devidedValue)
+
+    result = numerator /denometor
+
+    print (f"you entered: Numerator={numerator},denometor={denometor}")
+    print(f"result is:{result}")
+
+except ValueError:
+    print(f"Invalid Input")
+except ZeroDivisionError:
+    print(f"Can't Divided by Zero")
+except Exception as e:
+    print(f"An unexpected Error:{e}")
+
+
+
+
+    
+
