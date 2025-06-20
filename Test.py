ade000c55f98148db1757c78eacce1220df6d13a
muhaimin365 <ab.muhaimin95@gmail.com>
2025-06-20 02:11:52 -0700

    first commit

--- Test.py
+++ Test.py
@@ -1,23 +1,32 @@
-try:
-    UserInput=input("Enter value: ")
-    numerator = float(UserInput)
-    devidedValue = input("Enter Divided value: ")
-    denometor = float (devidedValue)
 
-    result = numerator /denometor
+class NagNumError(ValueError):
+    """Custom exception raised when a negative number is entered."""
+    def returnError(ValueError):
+        print(ValueError)
 
-    print (f"you entered: Numerator={numerator},denometor={denometor}")
-    print(f"result is:{result}")
+def get_positive_number():
+    """Ask the user for a number and raise a negative number error if needed."""
 
-except ValueError:
-    print(f"Invalid Input")
-except ZeroDivisionError:
-    print(f"Can't Divided by Zero")
-except Exception as e:
-    print(f"An unexpected Error:{e}")
+    while True:
+         try:
+             num_str = input("Please Enter zpositive Number: ")
+             number= float(num_str)
 
+             if number<0:
+                 raise NagNumError("Number Must be Positive")
+             return number
+         except ValueError:
+             print("Invalid input. Please enter a valid number.")
+         except NagNumError as e:
+            print(f"Error: {e}")
 
+if __name__ == "__main__":
+    try:
+        positive_Num = get_positive_number()
+        print(f"You entered a positive number: {positive_Num}")
+    except NagNumError as e:
+        print(f"Caught an error in the main script: {e}")
+    except Exception as e:
+        print("An unexpected error occurred.")
 
-
-    
 
